f89adba95f8ab734299aabe0ed789b3ed1987e61
GodT <Godtrb@Gmail.com>
2025-07-09 18:53:53 -0600

    Meds

--- Clinica.py
+++ Clinica.py
@@ -1,18 +1,53 @@
 class meds:
-    def __init__(self, Name, tabs,pill,syrup):
+    def __init__(self, Name, tabs=0,pill=0,syrup=0):
         self.Name = Name
         self.tabs = tabs
         self.pill = pill
         self.syrup = syrup
 def ShowAllMeds(MedList=[]):
+    contDisp=0
     for meds in MedList:
-        print("1)",meds.Name)
+        contDisp=contDisp+1
+        print(contDisp,")",meds.Name)
         print("Tableta:",meds.tabs," Pildora:", meds.pill," Jarabe:",meds.syrup)
 def MedTake (MedList=[]):
+    contDisp=0
     print("Que medicamento estaria tomando?")
     for meds in MedList:
-        print("1)", meds.Name)
+        print(contDisp,")",meds.Name)
 
     MedImput= int(input("Ingrese el numero del medicamento a tomar:"))
     if MedImput >0:
-        MedList[MedImput]()
+        print("En que presentacion estaria tomando ",MedList[MedImput-1].Name)
+        print("1)Tableta (",MedList[MedImput-1].tabs," en existencia)")
+        print("2)Pildora (",MedList[MedImput-1].pill," en existencia)")
+        print("3)Jarabe (",MedList[MedImput-1].syrup," en existencia)")
+        KindImput= int(input("Ingrese el numero del tipo de presentacion:"))
+        if KindImput >0:
+            if KindImput == 1:
+                MedList[MedImput-1].tabs = MedList[MedImput-1].tabs - 1
+            elif KindImput == 2:
+                MedList[MedImput-1].pill = MedList[MedImput-1].pill - 1
+            elif KindImput == 3:
+                MedList[MedImput-1].syrup = MedList[MedImput-1].syrup - 1
+        elif KindImput <1:
+            print("Dato Invalido")
+    elif MedImput < 1:
+        print("Dato Invalido")
+def MedAdd(MedList=[]):
+    print("Cual es el nombre del medicamento que desea agregar?")
+    medname= input("")
+    print("En que presentacion se estaria agregando?")
+    print("1)Tableta")
+    print("2)Pildora")
+    print("3)Jarabe")
+    KindImput = int(input("Ingrese el numero del tipo de presentacion:"))
+    if KindImput >0:
+        print("Cuantas unidades se estarian recibiendo?")
+        MedCuant= int(input(""))
+        if MedCuant >0:
+            if KindImput == 1:
+                NewMed= meds(medname,MedCuant)
+            elif KindImput == 2:
+                NewMed= meds(medname,0,MedCuant)
+            elif
