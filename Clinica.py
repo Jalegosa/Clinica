6d5060bb250a2d77ab1cb1f92a1bb56747016080
GodT <Godtrb@Gmail.com>
2025-07-09 18:25:02 -0600

    Meds

---
+++ Clinica.py
@@ -0,0 +1,13 @@
+class meds:
+    def __init__(self, Name, tabs,pill,syrup):
+        self.Name = Name
+        self.tabs = tabs
+        self.pill = pill
+        self.syrup = syrup
+def ShowAllMeds(MedList=[]):
+    for meds in MedList:
+        print("1)",meds.Name)
+        print("Tableta:",meds.tabs," Pildora:", meds.pill," Jarabe:",meds.syrup)
+def MedTake (MedList=[]):
+    for meds in MedList:
+        MedPick=
